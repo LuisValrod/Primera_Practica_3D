46c5169dce795acfc56448c98e0ccbc51bcfa1b5
LuisValrod <luisrodriguezvalido@gmail.com>
2023-12-01 20:57:06 +0000

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,32 @@
+import { Environment, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
+
+
+
+export const App = () => {
+  return (
+    <>
+      <div className='App'>
+       My first 3D Object
+      </div>
+      <Canvas className='canvas'>
+        <OrbitControls />
+        <Environment background preset='sunset'/>
+        <ambientLight />
+        <mesh> 
+          <boxGeometry />
+          <meshStandardMaterial color="#9922BB" />
+        </mesh>
+      </Canvas>
+
+    </>
+  )
+}
+
+/*Se crea un canvas donde va a vivir el entorno 3D. El Canvas contine un ambientLight para iluminar el objeto que puede tener 
+la propiedad intensity. 
+Deben haber los siguiente elementos:
+- mesh : Este elemento contiene geometria (boxGeometry), material (meshStandardMaterial, que ademas puede tener propiedad color)
+
+*/
